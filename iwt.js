ea2834a93b02f7f98c8af23bbc9520a27d789d9e
Heena <2012058@nec.edu.in>
2023-03-21 21:32:45 +0530

    Add files via upload

---
+++ iwt.js
@@ -0,0 +1,16 @@
+var a=50;
+var b=20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+document.write(a+b);
+function clickOf(){
+document.getElementById("main").innerHTML="hello";
+document.getElementById("main").style.color="green";
+document.getElementsByClassName("div")[0].style.backgroundColor="pink";
+const x=document.createElement("p");
+x.innerHTML="heeyyaaaaa"
+document.getElementById("main").appendChild(x);
+//document.write(typepf(x));
+}
